70c713791d8613c7a756f513427fd19042a06392
Bhuvanesh <bhuvaneshcmv@gmail.com>
2022-01-01 14:30:44 +0530

    Fixed Spawning level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,8 @@ public class GameManager : MonoBehaviour
     Transform levelParent;
     [SerializeField]
     LevelData levelData;
+    [SerializeField]
+    Vector3 deltaTilePos;
 
     private void Awake()
     {
@@ -25,17 +27,28 @@ public class GameManager : MonoBehaviour
     void CreateLevel()
     {
         Vector3 tilePos = levelData.boundaryTopLeft;
+        BgTile tempTile;
         for (int i = 0; i < 5000;i++)
         {
-            var tempTile = Instantiate(bgTileBorder,tilePos, Quaternion.identity);
+            if (tilePos.x == levelData.boundaryTopLeft.x || tilePos.x == levelData.boundaryBottomRight.x 
+                || tilePos.y == levelData.boundaryTopLeft.y || tilePos.y == levelData.boundaryBottomRight.y)
+            {
+                tempTile = Instantiate(bgTileBorder, tilePos, Quaternion.identity);
+            }
+            else
+            {
+                tempTile = Instantiate(bgTileNormal, tilePos, Quaternion.identity);
+            }
             tempTile.transform.SetParent(levelParent);
-            tempTile.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = i.ToString() ;
             bgTiles.Add(tempTile);
-            tilePos = new Vector3(tilePos.x += 0.25f, tilePos.y,tilePos.z);
+            tilePos = new Vector3(tilePos.x += deltaTilePos.x, tilePos.y,tilePos.z);
+
             if (tilePos.x > levelData.boundaryTopRight.x)
             {
-                tilePos.x = levelData.boundaryTopLeft.y;
-                tilePos = new Vector3(tilePos.x, tilePos.y-= 0.25f, tilePos.z);
+                tilePos.x = levelData.boundaryTopLeft.x;
+                tilePos = new Vector3(tilePos.x, tilePos.y -= deltaTilePos.y, tilePos.z);
+                if (tilePos.y < levelData.boundaryBottomLeft.y)
+                    return;
             }
         }
     }
