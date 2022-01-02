7c96bf3ab6f3fb864e6fc15d50a2513443e7d19a
Bhuvanesh <bhuvaneshcmv@gmail.com>
2022-01-02 20:19:47 +0530

    Added HUD

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HUD : MonoBehaviour
+{
+    private int score = 0;
+
+    [SerializeField]
+    TMP_Text text;
+
+    private void OnEnable()
+    {
+        Food.ateFood += IncreaseScore;
+    }
+    private void OnDisable()
+    {
+        Food.ateFood -= IncreaseScore;
+    }
+    void IncreaseScore(FoodType foodType)
+    {
+        if(foodType == FoodType.normal)
+            score++;
+        text.text = score.ToString();
+    }
+}
